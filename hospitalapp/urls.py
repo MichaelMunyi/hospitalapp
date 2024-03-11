a305a4d61ad44017b0163f6a224aaf6e3009303c
Munyimike9@gmail.com <Munyimike9@gmail.com>
2024-03-11 13:19:31 +0300

    first commit

---
+++ hospitalapp/urls.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from django.urls import path
+from hospitalapp import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('inner/', views.inner, name='inner'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('details/', views.appointments, name='appointments'),
+
+    path('member/', views.members, name='members'),
+    path('users/', views.users, name='users'),
+    path('products/', views.products, name='products'),
+    path('uploadimage/', views.upload_image, name='upload'),
+    path('showimage/', views.show_image, name='image'),
+    path('imagedelete/<int:id>', views.imagedelete),
+    path('adminHome/', views.adminhome, name='adminHome'),
+    path('pay/', views.pay, name='pay'),
+    path('stk/', views.stk, name='stk'),
+    path('token/', views.token, name='token'),
+]
